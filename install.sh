5aad9a522c20ad3a8ffb39ec4054a893d74d03f8
samedog <the.samedog@gmail.com>
2020-05-05 15:47:32 -0400

    added install script and fixed locatio of main script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cp -rf ./fgamemode /usr/bin/fgamemode
+chmod +x /usr/bin/fgamemode
+
+if [ ! -d "/usr/share/fgamemode" ];then
+	mkdir /usr/share/fgamemode
+fi
+cp -rf ./games.txt /usr/share/fgamemode/games.txt
+cp -rf ./fgamemode_daemon /etc/init.d/fgamemode_daemon
+chmod +x /etc/init.d/fgamemode_daemon
