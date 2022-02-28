29447a68cde6328d2084a5287845dc8a334c10f8
samedog <the.samedog@gmail.com>
2022-02-28 04:10:01 -0300

    some updates

--- install.sh
+++ install.sh
@@ -7,12 +7,14 @@ if [ ! -d "/usr/share/fgamemode" ];then
 	mkdir /usr/share/fgamemode
 fi
 cp -rf ./games.txt /usr/share/fgamemode/games.txt
-cp -rf ./fgamemode_daemon /etc/init.d/fgamemode_daemon
-chmod +x /etc/init.d/fgamemode_daemon
+cp -rf ./fgamemoded /etc/init.d/fgamemoded
+chmod +x /etc/init.d/fgamemoded
 
 #are we running a Pupplet or Pop!_os?
 if [[ ! -f "/etc/rc.d/PUPSTATE" ]];then
-    systemctl start fgamemode_daemon 
+    sudo update-rc.d fgamemoded defaults
+    systemctl enable fgamemoded
+    systemctl start fgamemoded
 else
-    /etc/init.d/fgamemode_daemon start
+    /etc/init.d/fgamemoded start
 fi
